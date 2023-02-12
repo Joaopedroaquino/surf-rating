d9343bb16c18362de2bddc009f7726b54802034a
joaopedroaquino <aqerhack@gmail.com>
2023-02-12 18:06:52 -0300

    Mock response data

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,5 +1,6 @@
 import { StormGlass } from "@src/clients/stormGlass";
 import axios from "axios";
+import stormGlassWeather3HoursFixtures from '@test/fixtures/stormglass_weather_3_hours.json';
 
 jest.mock('axios');
 
@@ -8,10 +9,10 @@ describe('StormGlass client', () => {
         const lat = -33.792726;
         const lng = 151.289824;
 
-        axios.get = jest.fn().mockResolvedValue({});
+        axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixtures);
         const stormGlass = new StormGlass(axios);
         const response = await stormGlass.fetchPoints(lat, lng);
-        expect(response).toEqual({})
+        expect(response).toEqual({});
 
     })
 });
--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,6 +1,9 @@
 import {  AxiosStatic } from "axios";
 
 export class StormGlass {
+    readonly stormGlassAPIparams = 
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+    readonly stormGlassAPISourcxe = 'noaa';
     constructor(protected request: AxiosStatic){}
     public async fetchPoints(lat: number, lng: number): Promise<{}>{
         return this.request.get('')
