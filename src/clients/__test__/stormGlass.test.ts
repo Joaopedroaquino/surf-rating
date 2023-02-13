70c666bc90842f9bb012beea7c86ba42d909bd1a
joaopedroaquino <aqerhack@gmail.com>
2023-02-12 23:09:44 -0300

    Add interfaces

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,6 +1,7 @@
 import { StormGlass } from "@src/clients/stormGlass";
 import axios from "axios";
 import stormGlassWeather3HoursFixtures from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormGlass_normalized_response_3_hours.json'
 
 jest.mock('axios');
 
@@ -12,7 +13,7 @@ describe('StormGlass client', () => {
         axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixtures);
         const stormGlass = new StormGlass(axios);
         const response = await stormGlass.fetchPoints(lat, lng);
-        expect(response).toEqual({});
+        expect(response).toEqual(stormGlassNormalized3HoursFixture);
 
     })
 });
--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,10 +1,32 @@
 import {  AxiosStatic } from "axios";
 
+export interface StormGlassPointSource {
+   [key:string]: number;
+
+
+}
+export interface StormGlassPoint{
+   readonly time: string;
+   readonly waveHeight: StormGlassPointSource;
+   readonly waveDirection: StormGlassPointSource;
+   readonly swellDirection:StormGlassPointSource;
+   readonly swellHeight: StormGlassPointSource;
+   readonly swellPeriod: StormGlassPointSource;
+   readonly windDirection: StormGlassPointSource;
+   readonly windSpeed: StormGlassPointSource;
+}
+export interface StormGlassForecastResponse {
+    hours: StormGlassPoint[];
+
+}
+
 export class StormGlass {
     readonly stormGlassAPIparams = 
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
     readonly stormGlassAPISourcxe = 'noaa';
+
     constructor(protected request: AxiosStatic){}
+
     public async fetchPoints(lat: number, lng: number): Promise<{}>{
         return this.request.get('')
 
