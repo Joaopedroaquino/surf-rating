bb28b8d0f8b18246d2fad4ae17aef741d6f7bef6
joaopedroaquino <aqerhack@gmail.com>
2023-02-12 23:17:27 -0300

    Add interfaces

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,34 +1,50 @@
-import {  AxiosStatic } from "axios";
+import { AxiosStatic } from "axios";
 
 export interface StormGlassPointSource {
-   [key:string]: number;
+    [key: string]: number;
 
 
 }
-export interface StormGlassPoint{
-   readonly time: string;
-   readonly waveHeight: StormGlassPointSource;
-   readonly waveDirection: StormGlassPointSource;
-   readonly swellDirection:StormGlassPointSource;
-   readonly swellHeight: StormGlassPointSource;
-   readonly swellPeriod: StormGlassPointSource;
-   readonly windDirection: StormGlassPointSource;
-   readonly windSpeed: StormGlassPointSource;
+export interface StormGlassPoint {
+    readonly time: string;
+    readonly waveHeight: StormGlassPointSource;
+    readonly waveDirection: StormGlassPointSource;
+    readonly swellDirection: StormGlassPointSource;
+    readonly swellHeight: StormGlassPointSource;
+    readonly swellPeriod: StormGlassPointSource;
+    readonly windDirection: StormGlassPointSource;
+    readonly windSpeed: StormGlassPointSource;
 }
 export interface StormGlassForecastResponse {
     hours: StormGlassPoint[];
 
 }
 
+export interface ForecastPoint {
+    time: string;
+    waveHeight: number;
+    waveDirection: number;
+    swellDirection: number;
+    swellHeight: number;
+    swellPeriod: number;
+    windDirection: number;
+    windSpeed: number;
+
+
+}
+
 export class StormGlass {
-    readonly stormGlassAPIparams = 
-    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+    readonly stormGlassAPIparams =
+        'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
     readonly stormGlassAPISourcxe = 'noaa';
 
-    constructor(protected request: AxiosStatic){}
+    constructor(protected request: AxiosStatic) { }
 
-    public async fetchPoints(lat: number, lng: number): Promise<{}>{
-        return this.request.get('')
+    public async fetchPoints(lat: number, lng: number): Promise<{}> {
+        const response = this.request.get<StormGlassForecastResponse>('');
 
+    }
+
+    private normalizeResponse(points: StormGlassForecastResponse): ForecastPoint[] {
     }
 }
