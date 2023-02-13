3b0ec51312531677e05f002c4e5adaaaecb0ae33
joaopedroaquino <aqerhack@gmail.com>
2023-02-12 23:40:04 -0300

    Implement normalize data

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -41,7 +41,7 @@ export class StormGlass {
     constructor(protected request: AxiosStatic) { }
 
     public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
-        const response = await this.request.get<StormGlassForecastResponse>('');
+        const response = await this.request.get<StormGlassForecastResponse>('https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}');
         return this.normalizeResponse(response.data);
 
     }
